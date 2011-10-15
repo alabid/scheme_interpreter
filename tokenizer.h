75d175c2d59e09d992bd0530ea4634707d3c48d1
Jie Lin <Jie@pool-208-179.res.carleton.edu>
2011-10-14 19:36:16 -0500

    add header file

---
+++ tokenizer.h
@@ -0,0 +1,22 @@
+enum TOKEN_TYPE{
+  booleanType, integerType, floatType, stringType, symbolType, openType, closeType
+};
+
+typedef struct __Value{
+  int type;
+  union {
+    int boolValue;
+    int intValue;
+    double dblValue;
+    char* stringValue;
+    char* symbolValue;
+    char open;
+    char close;
+    struct __ConsCell *cons;
+  };
+} Value;
+
+typedef struct __ConsCell{
+  struct __Value *car;
+  struct __Value *cdr;
+} ConsCell;
