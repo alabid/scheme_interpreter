bf0b4c8089057f927017a8bdb30b0be290a2ec91
Daniel Alabi <alabid@carleton.edu>
2011-10-23 21:30:24 -0500

    adding new files

---
+++ tokenizer/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+
+/*
+  main.c --
+  Tests our tokenize function.
+*/
+
+int main(int argc, char *argv[]) {
+  List *list; 
+  char* expression = (char *) malloc(sizeof(char) *256);
+  
+  while (fgets(expression, 256, stdin)) {
+    // printf("\n===========START=============\n");
+    list = tokenize(expression);
+    printToken(list);
+    // destroy and clean up
+    destroy(list);
+    // printf("\n=============END===========\n");
+  }
+  free(expression);
+  return 0;
+}
