20d5a6866ab579f70b8ff9062b86d15b7e743a09
Daniel Alabi <alabid@carleton.edu>
2011-11-14 07:15:44 -0600

    commiting changes

--- interpreter/main.c
+++ interpreter/main.c
@@ -6,7 +6,24 @@
 
 int main(int argc, char*argv[]) {
   Environment *topEnv = createTopFrame();
- 
+
+  if (argc > 1) {
+    int i;
+
+    for (i = 1; i < argc ; i++) {
+      FILE *file = fopen( argv[i], "r" );
+
+        /* fopen returns 0, the NULL pointer, on failure */
+        if (!file) {
+	  printf( "Could not load: %s\n", argv[i]);
+	  return 1;
+        } else {
+	  printf("loading... %s \n", argv[i]);
+	  loadFromFile(file, topEnv);
+        }
+    }
+  }
+
   interface(topEnv);
   
   destroyTopFrame(topEnv);
